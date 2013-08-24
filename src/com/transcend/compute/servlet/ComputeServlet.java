8d1f381656526ad9ad5a3961b0ea06d89f940317
John Gardner <jgardner@transcendcomputing.com>
2013-08-24 11:24:40 -0500

    Reformat source.

--- src/com/transcend/compute/servlet/ComputeServlet.java
+++ src/com/transcend/compute/servlet/ComputeServlet.java
@@ -13,39 +13,39 @@ import org.slf4j.LoggerFactory;
 import com.msi.tough.core.Appctx;
 
 public class ComputeServlet extends HttpServlet {
-	private final Logger logger = LoggerFactory.getLogger(this.getClass()
-			.getName());
-	private static final long serialVersionUID = 1L;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass()
+            .getName());
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * Process the GET request
-	 *
-	 * @param HttpServletRequest
-	 * @param HttpServletRespinse
-	 */
-	@Override
-	protected void doGet(final HttpServletRequest req,
-			final HttpServletResponse resp) throws ServletException,
-			IOException {
-		logger.debug("Into doGet");
-		doPost(req, resp);
-	}
+    /**
+     * Process the GET request
+     *
+     * @param HttpServletRequest
+     * @param HttpServletRespinse
+     */
+    @Override
+    protected void doGet(final HttpServletRequest req,
+            final HttpServletResponse resp) throws ServletException,
+            IOException {
+        logger.debug("Into doGet");
+        doPost(req, resp);
+    }
 
-	/**
-	 * Process doPost request
-	 *
-	 * @param HttpServletRequest
-	 * @param HttpServletRespinse
-	 */
-	@Override
-	protected void doPost(final HttpServletRequest req,
-			final HttpServletResponse resp) throws ServletException,
-			IOException {
-	       try {
-	            final ComputeServiceImpl impl = Appctx.getBean("computeService");
-	            impl.process(req, resp);
-	        } catch (final Exception e) {
-	            e.printStackTrace();
-	        }
-	}
+    /**
+     * Process doPost request
+     *
+     * @param HttpServletRequest
+     * @param HttpServletRespinse
+     */
+    @Override
+    protected void doPost(final HttpServletRequest req,
+            final HttpServletResponse resp) throws ServletException,
+            IOException {
+        try {
+            final ComputeServiceImpl impl = Appctx.getBean("computeService");
+            impl.process(req, resp);
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
